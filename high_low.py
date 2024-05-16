b1e2fe86bc7d76f22485d29428ade83817750ee3
Mrinal Roy <mkrdip@gmail.com>
2024-05-16 18:54:30 -0500

    Create high_low.py with starter code

---
+++ high_low.py
@@ -0,0 +1,12 @@
+import random
+
+NUM_ROUNDS = 3
+
+def main():
+    print("Welcome to the High-Low Game!")
+    print('--------------------------------')
+    
+    # TODO: Write your code here!!! :)
+
+if __name__ == "__main__":
+    main()
