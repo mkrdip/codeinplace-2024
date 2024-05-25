f86561f547ffeae6d6d01ac73f99a8b1c9f5764a
Mrinal Roy <mkrdip@gmail.com>
2024-05-25 12:30:15 -0500

    Add starter code to list_practice.py

---
+++ week6/list_practice.py
@@ -0,0 +1,15 @@
+def main():
+    # Create a list called `fruit_list` that contains the following fruits: 
+    # 'apple', 'banana', 'orange', 'grape', 'pineapple'.
+    
+    
+    # Print the length of the list.
+
+    
+    # Add 'mango' at the end of the list. 
+
+
+    # Print the updated list.
+    
+if __name__ == "__main__":
+    main()
